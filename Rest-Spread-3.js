6e3bfa1a781cf4a4056f5afe162fd3cf21344678
Aisha-Abdullahi <boyaaisha@gmail.com>
2023-04-07 09:34:53 +0300

    refactored

--- Rest-Spread-3.js
+++ Rest-Spread-3.js
@@ -5,12 +5,15 @@ const person = {
   age: 25
 };
 
-const id = person.id;
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
+// const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+
+const { id, firstName, lastName, age } = person
+const personInfo = { firstName, lastName, age };
 
 // console.log(id, personInfo);
 console.log(id, { ...personInfo });
